6a04bc5ba25e22bf30e93835a33b852a6528c228
Kadu100C <kadujgkdc@gmail.com>
2026-03-01 20:54:20 -0300

    upd

---
+++ Assets/scripts/Player_control.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Player_control : MonoBehaviour
+{
+
+    private Rigidbody2D _playerRigidbody2D;
+    public float _player_vel; //setar a velocidade do player dentro da unity
+    private Vector2 _playerDirection;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _playerRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _playerDirection = Vector2(input.GetAxisRaw("Horizontal"), input.GetAxisRaw("Vertical"));
+        _playerDirection.normalized;
+    }
+
+    void FixedUpdate()
+    {
+        _playerRigidbody2D.MovePosition(_playerRigidbody2D.position + _playerDirection * _player_vel * Time.deltaTime);
+        
+    }
+}
