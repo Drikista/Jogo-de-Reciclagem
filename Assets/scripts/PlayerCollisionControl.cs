8039003411d95fb35f0b77478d927e9686a61778
Kadu100C <kadujgkdc@gmail.com>
2026-03-01 23:25:26 -0300

    ta começando a funcionar :)

--- Assets/scripts/PlayerCollisionControl.cs
+++ Assets/scripts/PlayerCollisionControl.cs
@@ -2,16 +2,17 @@ using UnityEngine;
 
 public class PlayerCollisionControl : MonoBehaviour
 {
-[SerializeField] private int LixoReciclavelQTD = 0;
-
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    [SerializeField] private int GrabTrash = 0;
+    [SerializeField] private int LixoReciclavelQTD = 0;
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "LixoReciclavelTag") {
+        if (collision.gameObject.tag == "LixoReciclavelTag" && GrabTrash == 0 || collision.gameObject.tag == "LixoReciclavelTag" && GrabTrash == 1)
+        {
             LixoReciclavelQTD = LixoReciclavelQTD + 1;
-
+            Destroy(collision.gameObject);
+            GrabTrash = 1;
             
         }
     }
-   
+
 }
